58732a5f7307fb4cbfc523584a4fa192b4a9a0ce
marco3dog <62681327+marco3dog@users.noreply.github.com>
2023-03-24 12:16:18 -0700

    Nine buttons

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
+import React, { Component }  from 'react';
 export default function Square() {
-  return <button className="square">X</button>;
+  return(
+    <div>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+    </div>
+  ) 
 }
