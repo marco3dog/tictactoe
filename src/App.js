0d098816bd1fe72d2fc12710e0383a9cee48e541
marco3dog <62681327+marco3dog@users.noreply.github.com>
2023-03-26 14:25:52 -0700

    passed top level state to Board

--- src/App.js
+++ src/App.js
@@ -5,13 +5,10 @@ function Square({value, onSquareClick}) {
   return <button className="square" onClick={onSquareClick} >{value}</button>;
 }
 
-function Board() {
-
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+function Board({ xIsNext, squares, onPlay }) {
 
   function handleClick(i) {
-    if (squares[i] || calculateWinner(squares)) {
+    if (calculateWinner(squares) || squares[i]) {
       return;
     }
     const nextSquares = squares.slice();
@@ -20,8 +17,7 @@ function Board() {
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -58,11 +54,16 @@ export default function Game() {
 
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    // TODO
+  }
 
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+      <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
         <ol>{/*TODO*/}</ol>
